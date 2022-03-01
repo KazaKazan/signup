abf8353c6c8bbd6add2da97d53afbff63f124df9
KazaKazan <ahmetfiratusta@hotmail.com>
2022-03-01 10:12:24 +0300

    Add initial infoHandler logic.

--- script.js
+++ script.js
@@ -6,19 +6,48 @@ const formValidator = (() => {
 })();
 
 const infoHandler = (() => {
+    const infoPanel = document.getElementById("infoPanel");
+    const nameInfo = document.getElementById("infoName");
+    const lnameInfo = document.getElementById("infoLastName");
+    const mailInfo = document.getElementById("infoMail");
+    const phoneInfo = document.getElementById("infoPhone");
+    const pwdInfo = document.getElementById("infoPassword");
+
+    const infoButtons = [nameInfo, lnameInfo, mailInfo, phoneInfo, pwdInfo]
 
     const infoList = [
 
     ];
 
-    return{
+    function initialize(){
+        for(let i = 0; i < infoButtons.length; i++){
+            infoButtons[i].onmouseenter = () =>  showInfo(i)
+            infoButtons[i].onmouseleave = () =>  hideInfo()
+        };
+    };
 
+    function showInfo(infoIndex){
+        infoPanel.classList.remove("hidden");
+    };
+
+    function hideInfo(){
+        infoPanel.classList.add("hidden");
+    };
+
+    return{
+        initialize
     };
 })();
 
 const initializer = (() => {
 
-    return{
+    function start(){
+        infoHandler.initialize();
+    };
 
-    }
+    return{
+        start
+    };
 })()
+
+initializer.start();
